363e072ed868294438f2a2839b49876e0a9343c3
FragileDiamond <piranhagf@gmail.com>
2023-01-13 15:18:35 +0300

    Added `Gravatar` avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-
-  has_many :questions, dependent: :delete_all
-  has_many :asked_questions, class_name: 'Question', foreign_key: :author_id,
-    dependent: :nullify
+  has_secure_password
 
   before_save :downcase_nickname
 
@@ -13,7 +10,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true,
                        format: { with: /\A\w+\z/ }, length: { maximum: 40 }
 
-  has_secure_password
+  has_many :questions, dependent: :delete_all
+  has_many :asked_questions, class_name: 'Question', foreign_key: :author_id,
+           dependent: :nullify
+
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
 
   def downcase_nickname
     nickname.downcase!
